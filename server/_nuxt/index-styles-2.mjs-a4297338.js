8fd14a037e013781843e147c6d45c3e93020427b
Samina-tenso <tensammi@gmail.com>
2023-07-09 19:22:40 +0200

    Updates

--- server/_nuxt/index-styles-2.mjs-a4297338.js
+++
@@ -1,5 +0,0 @@
-const index_vue_vue_type_style_index_0_scoped_2ed14cf1_lang = ".profiles__list[data-v-2ed14cf1]{display:flex;flex-direction:row;flex-wrap:wrap;justify-content:center}.profiles__item[data-v-2ed14cf1]{list-style:none}.profiles__item[data-v-2ed14cf1]:not(last-child){padding-right:1rem}.profiles .button-wrapper[data-v-2ed14cf1]{align-items:center;display:flex;flex-direction:row;gap:1rem}.profiles .button[data-v-2ed14cf1]{background-color:#fff;border:none;display:flex;filter:drop-shadow(0 4px 8px rgba(0,0,0,.1));font-weight:300;justify-content:center;padding:1rem 3rem;width:4rem}.profiles .button[data-v-2ed14cf1]:hover{filter:drop-shadow(0 4px 2px rgba(0,0,0,.1))}.profiles .button--prev[data-v-2ed14cf1]{border-radius:2.5rem}.profiles .button--next[data-v-2ed14cf1]{border-radius:0 2.5rem 2.5rem 0}";
-export {
-  index_vue_vue_type_style_index_0_scoped_2ed14cf1_lang as default
-};
-//# sourceMappingURL=index-styles-2.mjs-a4297338.js.map
